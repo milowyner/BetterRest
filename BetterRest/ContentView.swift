4af5c4093e0e524669195867d1ae2486435c5a84
Milo Wyner <milowyner@gmail.com>
2021-07-18 14:54:20 -0700

    Connect CoreML model and SwiftUI code using part of my own solution before reading the tutorial's

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,9 +11,26 @@ struct ContentView: View {
     @State private var wakeUp = Date()
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var sleepTime = ""
+    @State private var alertShowing = false
     
     func calculateBedtime() {
-        
+        let model = SleepCalculator()
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        do {
+            let prediction = try model.prediction(input: SleepCalculatorInput(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount)))
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            self.sleepTime = formatter.string(from: sleepTime)
+            
+            alertShowing = true
+        } catch {
+            fatalError(error.localizedDescription)
+        }
     }
     
     var body: some View {
@@ -41,6 +58,9 @@ struct ContentView: View {
             .padding()
             .navigationTitle("BetterRest")
             .navigationBarItems(trailing: Button("Calculate", action: calculateBedtime))
+            .alert(isPresented: $alertShowing, content: {
+                Alert(title: Text("You should go to sleep at \(sleepTime)"))
+            })
         }
     }
 }
