308ea0550879737961cf9cc9ba2f11a1527e702f
Milo Wyner <milowyner@gmail.com>
2021-07-18 11:18:47 -0700

    Create basic interface using my own solution before reading tutorial's

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                DatePicker("Wake up time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                Stepper("\(sleepAmount, specifier: "%g") hours of sleep", value: $sleepAmount, in: 4...12, step: 0.25)
+                Stepper("\(coffeeAmount) cup\(coffeeAmount == 1 ? "" : "s") of coffee", value: $coffeeAmount, in: 1...10)
+            }
             .padding()
+        }
     }
 }
 
